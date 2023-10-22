131a9244231bd407f5887839a8f2308a41f456df
320-gogo32 <133937080+320-gogo32@users.noreply.github.com>
2023-10-22 16:50:18 +0900

    20231022_ボタンの変更

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -110,7 +110,7 @@
                                     <td class="align-middle"><a href="/item/detail/{{$item->id}}" class="btn btn-primary btn-sm mx-1">詳細</a></td>
                                     <td class="align-middle">
                                         @can('isAdmin')
-                                        <a href="/item/edit/{{$item->id}}" class="btn btn-primary btn-sm mx-1">編集</a></td>
+                                        <a href="/item/edit/{{$item->id}}" class="btn btn-warning btn-sm mx-1">編集</a></td>
                                         @endcan
                                 </tr>
                             @endforeach
